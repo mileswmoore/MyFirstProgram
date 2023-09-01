246b8a9286caccae9521b438c7ac55fc8a132ea3
mileswmoore <mileswmooreda@gmail.com>
2023-09-01 12:42:01 -0400

    My first commit

---
+++ src/FirstJava.java
@@ -0,0 +1,7 @@
+public class FirstJava {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        for(String s : args)
+            System.out.println(s);
+    }
+}
