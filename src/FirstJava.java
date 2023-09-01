3d42c045be755f455f38fe5e17aab51f52f99733
mileswmoore <mileswmooreda@gmail.com>
2023-09-01 13:06:31 -0400

    My first commit

--- src/FirstJava.java
+++ src/FirstJava.java
@@ -1,3 +1,9 @@
+/**
+ *  My first hello world program
+ *  I learned about command-line arguments also
+ * @version 09.01.2023
+ * @author 25moore
+ */
 public class FirstJava {
     public static void main(String[] args) {
         System.out.println("Hello World!");
@@ -5,3 +11,13 @@ public class FirstJava {
             System.out.println(s);
     }
 }
+
+
+/*
+This is a
+multi-line
+comment
+ */
+
+// this is a single line comment
+
